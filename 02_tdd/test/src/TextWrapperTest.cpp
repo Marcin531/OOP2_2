92a797d751ed0d9b5da712a41b910cb6f14268b9
Marcin <marcin531@gmail.com>
2019-10-14 16:16:19 +0200

    [red] checking columns

--- 02_tdd/test/src/TextWrapperTest.cpp
+++ 02_tdd/test/src/TextWrapperTest.cpp
@@ -11,3 +11,8 @@ class TextWrapper{
 TEST(TextWrapper, CreateInstance) { // pierwsze pole to nazwa klasy, ktora testuje, a drugie to co chce prztestowac
     auto wrapper = TextWrapper{};
 }
+
+TEST(TextWrapper, HasColumnsGetter) { // czy wrapper posiada metode umozliwiajaca wyciagniecie ilosci kolumn
+    auto wrapper=TextWrapper();
+EXPECT_EQ(10,wrapper.columns());
+}
