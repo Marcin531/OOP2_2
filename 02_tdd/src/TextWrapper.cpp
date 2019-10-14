306638cfa9396f8b476c89b74e35fadc7d6b4c97
Marcin <marcin531@gmail.com>
2019-10-14 17:22:58 +0200

    [green] Improve wrapText function - pass CanWrapLongerTextWithOneColumn test

--- 02_tdd/src/TextWrapper.cpp
+++ 02_tdd/src/TextWrapper.cpp
@@ -15,5 +15,14 @@ int TextWrapper::getNumberOfColumns() {
     return columns;
 }
 std::string TextWrapper::wrapText(std::string textToWrap) {
-    return textToWrap;
+    int i=0;
+    std::string temp;
+    while (textToWrap[i+1]){
+        temp=textToWrap.substr(i+this->columns);
+        textToWrap=textToWrap.substr(0,i+this->columns);
+        textToWrap[i+this->columns]='\n';
+        textToWrap=textToWrap.append(temp);
+        std::cout<<textToWrap;
+
+    }
 }
